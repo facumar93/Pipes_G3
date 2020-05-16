601f3531005413556d622507dd7370b901ed659c
facumar93 <53872293+facumar93@users.noreply.github.com>
2020-05-15 21:55:07 -0300

    Primer Commit

---
+++ src/Program/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CompAndDel.Filters;
+using CompAndDel.Pipes;
+
+namespace CompAndDel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Se instancia PictureProvide que abrirá una imagen con un Método.
+            PictureProvider p = new PictureProvider();
+            //GetPicture retornará una instancia de Picture.
+            IPicture pic = p.GetPicture("C:/Users/estudiante.fit/Pipes_G3/matrix.png");
+
+            //Necesario instanciar los filtros, porque los "Pipes" se instancian del ultimo al primero.
+            IFilter filterGreyscale= new FilterGreyscale();
+            IFilter filterNegative = new FilterNegative();
+            
+            //
+            IPipe ipip2 = new PipeSerial(filterNegative, new PipeNull());
+            IPipe ipip1 = new PipeSerial(filterGreyscale, ipip2);
+
+            ipip2.Send(pic);
+
+            
+        }
+    }
+}
