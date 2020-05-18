0cc9cec595843b7723f01e9b3907d6493439f320
anakaprielian1 <62777141+anakaprielian1@users.noreply.github.com>
2020-05-17 22:59:38 -0300

    hasta ejercicio 3

---
+++ src/Library/Filters/FilterTwiter.cs
@@ -0,0 +1,21 @@
+namespace CompAndDel.Filters
+{
+   public class FilterTwitter : IFilter
+    {
+        
+        
+        public IPicture Filter (IPicture image)
+        {
+            var twitter = new TwitterImage(ConsumerKey, ConsumerKeySecret, AccessToken, AccessTokenSecret);
+            System.Console.WriteLine(twitter.PublishToTwitter("new filter added",$"../imageWithFilter{contador}.JPG"));
+            return image;
+            
+        }
+
+        private int contador = 1;
+        public string ConsumerKey = "g7rkPB5uI2xOqELAhlNrorSU4";
+        public string ConsumerKeySecret = "8hOTyS71GrTH9Ool3rXykAJRY5AmgSPiy78b1wYUPcvfIzXeEc";
+        public string AccessToken = "1396065818-8vnV9HJFW5ArcfFg2zE9hLA68CZYFXO8Cjv6o2E";
+        public string AccessTokenSecret = "675fHmUzeaPajtj3pO64w5xd3p9YI3kco7kSvKhzeEvYe";
+    }
+}
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -15,11 +15,28 @@ namespace CompAndDel
             IFilter filterGreyscale= new FilterGreyscale();
             IFilter filterNegative = new FilterNegative();
             IFilter filterProvider = new FilterProvider();
+            IFilter filterTwiter = new FilterTwitter();
+            
+            /* 
+            //EJERCICIO 2
             
             IPipe pipeNull = new PipeNull();
             IPipe pipeProvider2 = new PipeSerial(filterProvider, pipeNull);
             IPipe pipeSerial2 = new PipeSerial(filterNegative,pipeProvider2);
             IPipe pipeProvider1 = new PipeSerial(filterProvider,pipeSerial2);
+            IPipe pipeSerial1 = new PipeSerial(filterGreyscale,pipeProvider1);
+            
+            pipeSerial1.Send(pic);
+            */
+
+            //EJERCICIO 3
+
+            IPipe pipeNull = new PipeNull();
+            IPipe pipeTwiter2 = new PipeSerial(filterTwiter, pipeNull);
+            IPipe pipeProvider2 = new PipeSerial(filterProvider, pipeTwiter2);
+            IPipe pipeSerial2 = new PipeSerial(filterNegative,pipeProvider2);
+            IPipe pipeTwiter1 = new PipeSerial(filterTwiter,pipeSerial2);
+            IPipe pipeProvider1 = new PipeSerial(filterProvider, pipeTwiter1);
             IPipe pipeSerial1 = new PipeSerial(filterGreyscale,pipeProvider1);
 
             pipeSerial1.Send(pic);
